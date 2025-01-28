2740629ce9dab746202ea497b3ce94014b51f9af
lpoleosi <76439903+lpoleosi@users.noreply.github.com>
2025-01-28 16:51:30 -0400

    log4j2

--- bank/bank/src/main/java/com/msTransfer/bank/controllers/CuentaBancariaController.java
+++ bank/bank/src/main/java/com/msTransfer/bank/controllers/CuentaBancariaController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import com.msTransfer.bank.entities.CuentaBancaria;
 import com.msTransfer.bank.services.CuentaBancariaService;
 
@@ -23,13 +24,17 @@ import jakarta.persistence.EntityNotFoundException;
 public class CuentaBancariaController {
 	 	@Autowired
 	    private CuentaBancariaService cuentaBancariaService;
+	 	
+	 	 private static final Logger logger = LogManager.getLogger(CuentaBancariaController.class);
 
 	 	 @PostMapping
 	     public ResponseEntity<CuentaBancaria> crearCuenta(@RequestBody CuentaBancaria request) {
 	         try {
+	        	 logger.info("Creating new account: {}", request);
 	        	 CuentaBancaria  cta = cuentaBancariaService.createAccount(request);
 	             return new ResponseEntity<>(cta, HttpStatus.CREATED);
 	         } catch (EntityNotFoundException e) {
+	        	 logger.error("Error creating account: Client not found", e);
 	        	 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	         }
 	     }
